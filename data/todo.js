a48b2ecec3d896d3cb8a1a8eb68da09d981b3c20
Daniella Marks <marks.daniella@gmail.com>
2021-05-13 13:51:51 -0700

    db schema

---
+++ data/todo.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    task: 'wash the dishes',
+    completed: false,
+    shared: false,
+    userId: 4,
+    userName: 'Chris' // only on GETs
+  },
+  {
+    task: 'wash the dishes',
+    completed: false,
+    shared: false,
+    userId: 4,
+    userName: 'Chris' // only on GETs
+  }, 
+  {
+    task: 'wash the dishes',
+    completed: false,
+    shared: false,
+    userId: 4,
+    userName: 'Chris' // only on GETs
+  }];
